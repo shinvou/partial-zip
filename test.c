e379ed6a62a3a1091773831f19e44a53e8d6f9a2
Timm Kandziora <hello@shinvou.org>
2012-02-20 13:57:24 +0100

    added some apple-geeky-stuff

--- test.c
+++ test.c
@@ -10,34 +10,33 @@ void callback(ZipInfo* info, CDFile* file, size_t progress) {
 }
 
 int main(int argc, char* argv[]) {
-	ZipInfo* info = PartialZipInit("file://test.zip");
+	ZipInfo* info = PartialZipInit("http://appldnld.apple.com/iPhone4/041-3417.20111215.Slnt4/iPhone4,1_5.0.1_9A406_Restore.ipsw"); // iPhone 4s 5.0.1 ipsw
 	if(!info)
 	{
-		printf("Cannot find /tmp/test.zip\n");
+		printf("Cannot find file.\n");
 		return 0;
 	}
-
+    
 	PartialZipSetProgressCallback(info, callback);
-
-	CDFile* file = PartialZipFindFile(info, "test.txt");
+    
+	CDFile* file = PartialZipFindFile(info, "Firmware/all_flash/all_flash.n94ap.production/manifest"); // Just a list of files
 	if(!file)
 	{
-		printf("Cannot find test.txt in /tmp/test.zip\n");
+		printf("Cannot show info of file.\n");
 		return 0;
 	}
-
+    
 	unsigned char* data = PartialZipGetFile(info, file);
 	int dataLen = file->size; 
-
+    
 	PartialZipRelease(info);
-
+    
 	data = realloc(data, dataLen + 1);
 	data[dataLen] = '\0';
 	
 	printf("%s\n", data);
-
+    
 	free(data);
-
+    
 	return 0;
 }
-
